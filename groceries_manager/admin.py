164af978e21fb573c9b43fc0879eb62357147273
adriangg <adrian.garcia@vanadys.com>
2018-05-15 21:32:03 +0200

    model modifications and ticket-product stuff

--- groceries_manager/admin.py
+++ groceries_manager/admin.py
@@ -1,3 +1,22 @@
+
 from django.contrib import admin
+from .models import Product_category, Product_group, Product_subgroup, Product, Supermarket, Ticket
 
 # Register your models here.
+class ProductInline(admin.TabularInline):
+    model = Product
+    extra = 3
+    
+
+class TicketAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['date']}),
+        ('Date information',    {'fields': ['date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ProductInline]
+    # list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['date']
+    search_fields = ['date']
+    
+
+admin.site.register(Ticket, TicketAdmin)
--- groceries_manager/models.py
+++ groceries_manager/models.py
@@ -18,6 +18,18 @@ class Product_subgroup(models.Model):
     name = models.CharField(max_length=200)
 
 
+class Country(models.Model):
+    id = models.AutoField(primary_key=True)
+    iso2 = models.CharField(max_length=2)
+    name = models.CharField(max_length=200)
+
+
+class Ticket(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_supermarket = models.IntegerField(default=1)
+    date = models.DateTimeField('Bought on this day')
+
+
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -26,22 +38,21 @@ class Product(models.Model):
     subgroup = models.IntegerField(default=None)
 
 
+class Ticket_line(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    id_ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    unit_price = models.FloatField(default=None)
+    quantity = models.IntegerField(default=1)
+    weight = models.IntegerField(default=1)
+
+
 class Supermarket(models.Model):
     id = models.AutoField(primary_key=True)
     country = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
-    zip = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
     franchise = models.CharField(max_length=50)
     name = models.CharField(max_length=200)
 
-
-class Ticket(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_product = models.IntegerField(default=1)
-    id_supermarket = models.IntegerField(default=1)
-    unit_price = models.FloatField(default=None)
-    quantity = models.IntegerField(default=1)
-    weight = models.IntegerField(default=1)
-    bought_on = models.DateTimeField('Bought on this day')
-    bought_on = models.DateTimeField('Bought on this day')
 
